fcc3426574f9ccd0f3f9939273cc57d42aa34ab4
cheukl fung ng <vicksonfung@gmail.com>
2019-03-08 15:35:39 +0800

    Delete '/' in production js

--- config/env/production.js
+++ config/env/production.js
@@ -251,7 +251,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
-      'https://wecarebill.herokuapp.com/'
+      'https://wecarebill.herokuapp.com'
     ],
 
 
