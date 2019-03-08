8999f98f4af1cd47d4cd33a9c653d38a040154a8
cheukl fung ng <vicksonfung@gmail.com>
2019-03-08 15:31:28 +0800

    first commit

---
+++ api/controllers/MainController.js
@@ -0,0 +1,15 @@
+/**
+ * MainController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    fulfill: async function (req, res) {
+        return;
+    }
+  
+
+};
+
---
+++ api/policies/fulfillment.js
@@ -0,0 +1,29 @@
+'use strict';
+ 
+//const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+const {dialogflow} = require('actions-on-google');
+
+const app = dialogflow();
+
+var surgery;
+var price;
+var doctorName;
+var hospital;
+
+app.intent('user provides surgery', (conv, params) => {
+    surgery = params.surgery;
+  	
+});
+app.intent('user provides doctor name', (conv, params) => {
+    doctorName = params.doctorName;
+
+});
+app.intent('user provides hospital', (conv, params) => {
+    
+    hospital = params.hospital;
+
+});
+//exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
+module.exports = app;
