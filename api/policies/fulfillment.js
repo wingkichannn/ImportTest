a26eee032dc1fc1bf6af7af9531d719448a319f0
cheukl fung ng <vicksonfung@gmail.com>
2019-03-12 19:26:13 +0800

    Fisrt attempt to test the makeOutput function in fulfillment.js

--- api/policies/fulfillment.js
+++ api/policies/fulfillment.js
@@ -12,6 +12,37 @@ var price;
 var doctorName;
 var hospital;
 
+const gastroscopy_summary= { 
+    "A" : gastroscopy_A,
+    "B" : gastroscopy_B,
+    "C" : gastroscopy_C,
+    
+};
+const gastroscopy_A= {
+    "A1" : {
+        "name" : "沒息肉切除", 
+        "price" : 0
+    },
+    "A2" :{
+        "name" : "<=3有息肉切除 <=1cm",
+        "price" : 4700
+    },
+    "A3" :{
+        "name" : ">3息肉切除 <=1cm",
+        "price" : 6800
+    },
+
+    "A4" :{
+        "name": "大粒息肉 >1cm",
+        "price" : -1
+    }
+},
+
+const gastroscopy_B = ["一般胃鏡", "自體熒光成像", "超細胃鏡5mm(小孩/老人)", "膠囊內鏡"];
+
+const gastroscopy_C = ["沒有併發症/複雜/特別風險/另加手術", "中風險", "有併發症/複雜/另加手術", "急症"];
+
+
 app.intent('user provides surgery', (conv, params, request) => {
     surgery = params.surgery;
     console.log("The surgery is "+surgery);
@@ -28,8 +59,19 @@ app.intent('user provides hospital', (conv, params) => {
     
     hospital = params.hospital;
     console.log("The doctor name is "+hospital);
+    makeOutputMessage(surgery);
     return conv.close(req.queryResult.fulfillmentText);
 
 });
+app.intent('user modifies details', (conv, params) => {
+   
+});
+function makeOutputMessage(surgery){
+    var outputMessage;
+    for(var key in gastroscopy_summary){
+        console.log(key);
+    }
+    
+};
 //exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 module.exports = app;
