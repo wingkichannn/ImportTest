10e649d645a874c7cdf5535eb922c967c00ec2ba
cheukl fung ng <vicksonfung@gmail.com>
2019-03-13 02:25:09 +0800

    Try to change object to array

--- api/policies/fulfillment.js
+++ api/policies/fulfillment.js
@@ -15,44 +15,50 @@ var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 
 
 
-// var gastroscopy_A= {
-//     "A1" : {
-//         "name" : "沒息肉切除", 
-//         "price" : 0
-//     },
-//     "A2" :{
-//         "name" : "<=3有息肉切除 <=1cm",
-//         "price" : 4700
-//     },
-//     "A3" :{
-//         "name" : ">3息肉切除 <=1cm",
-//         "price" : 6800
-//     },
+var gastroscopy_A= {
+    "A1" : {
+        "name" : "沒息肉切除", 
+        "price" : 0
+    },
+    "A2" :{
+        "name" : "<=3有息肉切除 <=1cm",
+        "price" : 4700
+    },
+    "A3" :{
+        "name" : ">3息肉切除 <=1cm",
+        "price" : 6800
+    },
 
-//     "A4" :{
-//         "name": "大粒息肉 >1cm",
-//         "price" : -1
-//     }
-// };
-// var gastroscopy_B = {
-//    "B1":{
-//        "name": "一般胃鏡",
-//        "price":-1
-//     },
-//     "B2":{
-//         "name":"自體熒光成像",
-//         "price":-1
-//     },
-//     "B3":{
-//         "name":"超細胃鏡5mm(小孩/老人)",
-//         "price":-1
+    "A4" :{
+        "name": "大粒息肉 >1cm",
+        "price" : -1
+    }
+};
+var gastroscopy_B = {
+   "B1":{
+       "name": "一般胃鏡",
+       "price":-1
+    },
+    "B2":{
+        "name":"自體熒光成像",
+        "price":-1
+    },
+    "B3":{
+        "name":"超細胃鏡5mm(小孩/老人)",
+        "price":-1
 
-//     } , 
-//     "B4":{
-//         "name":"膠囊內鏡",
-//         "price":-1
-//     }
-// };
+    } , 
+    "B4":{
+        "name":"膠囊內鏡",
+        "price":-1
+    }
+};
+var gastroscopy_summary= { 
+        "A" : gastroscopy_A,
+        "B" : gastroscopy_B,
+        
+        
+    };
 
 // var gastroscopy_C = ["沒有併發症/複雜/特別風險/另加手術", "中風險", "有併發症/複雜/另加手術", "急症"];
 
@@ -61,24 +67,25 @@ var abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
 // var gastroscopy_C = ["沒有併發症/複雜/特別風險/另加手術", "中風險", "有併發症/複雜/另加手術", "急症"];
 
 ////////////////// convert the object to an array.//////////////////////////////////////
-// var gastroscopy_A_array = [];
-// var gastroscopy_B_array = [];
-// for(var key in gastroscopy_A){
-//     gastroscopy_A_array.push(gastroscopy_A[key]);
-// }
-// for(var key in gastroscopy_B){
-//     gastroscopy_B_array.push(gastroscopy_B[key]);
-// }
+var gastroscopy_A_array = [];
+var gastroscopy_B_array = [];
+for(var key in gastroscopy_A){
+    gastroscopy_A_array.push(gastroscopy_A[key]);
+}
+console.log(">>>>>>>>>>>>>>>>>>>>>>"+gastroscopy_A_array);
+for(var key in gastroscopy_B){
+    gastroscopy_B_array.push(gastroscopy_B[key]);
+}
 ///////////////////////////////////////////////////////////////////////////////////
-var gastroscopy_B = ['一般胃鏡', '自體熒光成像', '超細胃鏡5mm(小孩/老人)', '膠囊內鏡'];
+// var gastroscopy_B = ['一般胃鏡', '自體熒光成像', '超細胃鏡5mm(小孩/老人)', '膠囊內鏡'];
 
-var gastroscopy_C = ['沒有併發症/複雜/特別風險/另加手術', '中風險', '有併發症/複雜/另加手術', '急症'];
-var gastroscopy_summary= { 
-    "B" : gastroscopy_B,
-    "C" : gastroscopy_C,
+// var gastroscopy_C = ['沒有併發症/複雜/特別風險/另加手術', '中風險', '有併發症/複雜/另加手術', '急症'];
+// var gastroscopy_summary= { 
+//     "B" : gastroscopy_B,
+//     "C" : gastroscopy_C,
     
     
-};
+// };
 
 //////////////////////Code for each Intent////////////////////////////////////
 
@@ -107,13 +114,14 @@ app.intent('user provides price', (conv,params) => {
     price = params.price;
     console.log("The price is "+ price);
     var output = "所以，你地案例為下：\n"
-    console.log(output);
+    // console.log(output);
     console.log("@@@@@@@@");
-    console.log(gastroscopy_summary.B);
+    // console.log(gastroscopy_summary.B);
     for(var key in gastroscopy_summary){
         var temp = gastroscopy_summary[key];
         console.log("*******"+temp);
        console.log(temp[1].name);
+       const optionsList = list.map(item => Object.values(item)[0]);
     }
     var count = 0;
     for(var key in gastroscopy_summary){
