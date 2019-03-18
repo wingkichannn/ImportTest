0b8fb4196c060d8421c34a7ce3e732eeca81f250
cheukl fung ng <vicksonfung@gmail.com>
2019-03-18 19:23:10 +0800

    Change intent to agent function and connect firebase

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -8,59 +8,103 @@ module.exports = function() {
   
     const agent = new WebhookClient({request: req, response: res});
   
-    // function welcome (agent) {
-    //   agent.add(`Welcome to my agent!`);
-    // }
+    async function welcome (agent) {
+      agent.add(`Welcome to my agent!`);
+    }
   
-    // function fallback (agent) {
-    //   agent.add(`I didn't understand`);
-    //   agent.add(`I'm sorry, can you try again?`);
-    // }
-  
-    let intentMap = new Map();
-    // intentMap.set('Default Welcome Intent', welcome);
-    // intentMap.set('Default Fallback Intent', fallback);
-    // // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
-    // // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
-    // agent.handleRequest(intentMap);
-
-    intentMap.set('user provides surgery', (agent) => {
+    async function fallback (agent) {
+      agent.add(`I didn't understand`);
+      agent.add(`I'm sorry, can you try again?`);
+    }
+    async function surgery(agent){
         let conv = agent.conv();
-        console.log(":::::::::::::::::::::::");
-        console.log(conv);
         let params = agent.parameters;
-        surgery = params.surgery;
-        console.log("The surgery is "+surgery);
-      
-        return agent.add('請輸入負責手術的醫生名字，如不知道請輸入"0"**');
+        doctorName = params.doctorName;
+        console.log("The doctor name is "+doctorName);
+        return agent.add('請問醫院名稱**');
         
-    });
-    intentMap.set('user provides doctor name', (agent) => {
-        let conv = agent.conv();
+    }
+    async function doctorName(agent){
         let params = agent.parameters;
         doctorName = params.doctorName;
         console.log("The doctor name is "+doctorName);
         // getFulfillmentText(agent);
         console.log("Surgery in doctor name intent: ");
-        return agent.add('請問醫院名稱**');
-        
-    });
-    intentMap.set('user provides hospital', (agent) => {
-        let conv = agent.conv();
+        agent.add('請問醫院名稱**');
+
+    }
+    async function noDoctorName(agent){
+
+    }
+    async function hospital(agent){
         let params = agent.parameters;
         hospital = params.hospital;
         console.log("The hospital is "+hospital);
         //console.log(">>>>"+)
-        console.log("INput context is: "+ req.body.outputContexts[2].surgery);
+       // console.log("INput context is: "+ req.body.outputContexts[2].surgery);
        
-        return conv.close('請輸入全單價錢(包括所有醫生/醫院收費)**');
-    
-    });
-    intentMap.set('user provides price', (agent) => {
-        let conv = agent.conv();
+        agent.add('請輸入全單價錢(包括所有醫生/醫院收費)**');
+
+    }
+    async function price(agent){
         let params = agent.parameters;
         price = params.price;
         console.log("The price is "+ price);
+        agent.add("The price is "+ price);
+
+    }
+  
+    let intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('user provides surgery', surgery);
+    intentMap.set('user provides doctor name', doctorName);
+    intentMap.set('user provides hospital', hospital);
+    intentMap.set('user provides price', price);
+    intentMap.set('user does not provide doctor name', noDoctorName);
+    intentMap.set('user wants to see doctor list', doctorlist);
+    intentMap.set('follow up', followUp);
+    // // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+    // // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
+    // agent.handleRequest(intentMap);
+
+    // intentMap.set('user provides surgery', (agent) => {
+    //     let conv = agent.conv();
+    //     console.log(":::::::::::::::::::::::");
+    //     console.log(conv);
+    //     let params = agent.parameters;
+    //     surgery = params.surgery;
+    //     console.log("The surgery is "+surgery);
+      
+    //     return agent.add('請輸入負責手術的醫生名字，如不知道請輸入"0"**');
+        
+    // });
+    // intentMap.set('user provides doctor name', (agent) => {
+    //     let conv = agent.conv();
+    //     let params = agent.parameters;
+    //     doctorName = params.doctorName;
+    //     console.log("The doctor name is "+doctorName);
+    //     // getFulfillmentText(agent);
+    //     console.log("Surgery in doctor name intent: ");
+    //     return agent.add('請問醫院名稱**');
+        
+    // });
+    // intentMap.set('user provides hospital', (agent) => {
+    //     let conv = agent.conv();
+    //     let params = agent.parameters;
+    //     hospital = params.hospital;
+    //     console.log("The hospital is "+hospital);
+    //     //console.log(">>>>"+)
+    //     console.log("INput context is: "+ req.body.outputContexts[2].surgery);
+       
+    //     return conv.close('請輸入全單價錢(包括所有醫生/醫院收費)**');
+    
+    // });
+    // intentMap.set('user provides price', (agent) => {
+    //     let conv = agent.conv();
+    //     let params = agent.parameters;
+    //     price = params.price;
+    //     console.log("The price is "+ price);
         // for(var key in surgery_summary)break;
         // var temp = surgery_summary[key];
         // console.log("11111111."+temp);
@@ -79,14 +123,14 @@ module.exports = function() {
         //     output += (abc[count] +": "+gastroscopy_summary[key][1].name+"\n");
         //     count++;
         // }
-        // console.log(output);
-        return conv.close("Display the surgery summary");
+        // // console.log(output);
+        // return conv.close("Display the surgery summary");
     
-    });
-    intentMap.set('user modifies details', (agent) => {
-        let conv = agent.conv();
+    // });
+    // intentMap.set('user modifies details', (agent) => {
+    //     let conv = agent.conv();
        
-    });
+    // });
 
     agent.handleRequest(intentMap);
   }
