88b43ab1e3c8d6e1b5f66de7cfc0f30e9832dc69
cheukl fung ng <vicksonfung@gmail.com>
2019-03-19 14:12:22 +0800

    Add a if to determine whether app is initialized or not

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -7,14 +7,20 @@ module.exports = function () {
 
     // connect firebase
     const agent = new WebhookClient({ request: req, response: res });
+
     var admin = require('firebase-admin');
+
     var serviceAccount = require(sails.config.appPath + '/wecarebill-92132-firebase-adminsdk-7usxj-6240df0e36.json');
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: 'https://wecarebill-92132.firebaseio.com'
-  
-    });
-  
+
+    if (!firebase.apps.length) {
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+            databaseURL: 'https://wecarebill-92132.firebaseio.com'
+
+        });
+    }
+
+
     var db = admin.firestore();
 
     // FUNCTIONS FOR INTENTS
