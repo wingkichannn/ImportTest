10ddcdc5b36dfa8b1ad031d8a871da9da8b75c1a
cheukl fung ng <vicksonfung@gmail.com>
2019-03-19 14:06:04 +0800

    remove index2

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -9,15 +9,15 @@ module.exports = {
     fulfill: async function (req, res) {
         return res.fulfillment();
     },
-    index2: async function (req, res) {
-        var admin = require('firebase-admin');
-        var serviceAccount = require(sails.config.appPath + '/wecarebill-92132-firebase-adminsdk-7usxj-6240df0e36.json');
-        admin.initializeApp({
-          credential: admin.credential.cert(serviceAccount),
-          databaseURL: 'https://wecarebill-92132.firebaseio.com'
+    // index2: async function (req, res) {
+    //     var admin = require('firebase-admin');
+    //     var serviceAccount = require(sails.config.appPath + '/wecarebill-92132-firebase-adminsdk-7usxj-6240df0e36.json');
+    //     admin.initializeApp({
+    //       credential: admin.credential.cert(serviceAccount),
+    //       databaseURL: 'https://wecarebill-92132.firebaseio.com'
       
-        })
-    }
+    //     })
+    // }
     
 };
 
