9678f3fcd2a228f14841454ff95561d9aea9800c
cheukl fung ng <vicksonfung@gmail.com>
2019-03-21 02:43:33 +0800

    Try restart flow

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -28,7 +28,7 @@ module.exports = function () {
         let params = agent.parameters;
         let surgery = params.surgery;
         console.log("The surgery name is " + surgery);
-        agent.add('請問醫生名稱**');
+        agent.add('請問醫生名稱，如不知道，請輸入"不知道"**');
     }
 
     // Show baseline case + all specific options with price.
@@ -174,7 +174,7 @@ module.exports = function () {
         var contextSurgery = outputContexts.parameters.surgery;
         var surgery = await db.collection('surgery').doc(contextSurgery).get();
         var ChineseName = surgery.data()['內容'];
-        agent.add(await getDoctorList());
+        agent.add(await getDoctorList()+"，但個別醫生收費有異，而醫療服務收費昤會作出修改，而病人情況因人而異，如有需要請向你的醫生請教。本平台只搜集病人個案，會力求資料正確，內容只供參考之用，未得同意，不得作其他商業用途，唯最終資料準確性，請諮詢醫生及專業人仕。");
         async function getDoctorList() {
             var countNum = 1;
             var doctorList = "而根據其他病人分享的資料，做"+ChineseName+"且收費接近中位數的醫生有: ";
@@ -232,6 +232,17 @@ module.exports = function () {
         agent.add("The price is " + price);
 
     }
+    async function followup(agent){
+
+    }
+    async function restart(agent){
+        const response = {
+            followupEventInput: {
+              name: "Welcome",
+          }
+         }
+         return agent.response_.status(200).send(response);
+    }
 
     let intentMap = new Map();
     intentMap.set('Default Welcome Intent', welcome);
@@ -239,6 +250,8 @@ module.exports = function () {
     intentMap.set('user provides surgery', surgery);
     intentMap.set('user provides doctor name', doctorName);
     intentMap.set('user does not provide doctor name', noDoctorName)
+    intentMap.set('followup', followup);
+    intentMap.set('restart', restart);
     intentMap.set('user provides hospital', hospital);
     intentMap.set('user provides price', price);
     intentMap.set('user wants to see doctor list', doctorList);
