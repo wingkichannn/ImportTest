df40f192d9193e92c2c860748462d3268ed37061
jenniferjenniferjen <16226437@life.hkbu.edu.hk>
2019-04-08 12:35:05 +0800

    added functions related to another sheet

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -34,31 +34,36 @@ module.exports = {
 
         if (req.method == 'GET')
             return res.view('main/upload');
-
-        req.file('surgeryno').upload({ maxBytes: 10000000 }, async function whenDone(err, uploadedFiles) {
-            if (err) { return res.serverError(err); }
+            const csv = require("fast-csv")
+            // var surgeryno = require("fast-csv");
+            // var option = require("fast-csv");
+            var db = sails.firebaseAdmin.firestore();
+            var uploadedFiles = req.file('surgeryno').upload({ maxBytes: 10000000 });
+                //, async function whenDone(err, uploadedFiles) {
+            //if (err) { return res.serverError(err); }
             if (uploadedFiles.length === 0) { return res.badRequest('No file was uploaded'); }
 
             var db = sails.firebaseAdmin.firestore();
-            var surgeryno = require("fast-csv");
-            var option = require("fast-csv");
+            // var surgeryno = require("fast-csv");
+            // var option = require("fast-csv");
             var index = 0;
 
             var batch = db.batch();
 
-            surgeryno.fromPath(uploadedFiles[0].fd, { headers: false }).on("data", function (data) {
+            csv.fromPath(uploadedFiles[0].fd, { headers: false }).on("data", function (data) {
                 if (index == 0) {
                     index++;
                     return;
                 }
 
                 //  console.log()
-                //  console.log(data);
+             console.log(data);
                 if (data[0]) {
 
                     batch.set(db.collection('surgery').doc(data[0]), {
                         "content": data[1],
                         "內容": data[2] ? data[2] : '',
+                        "specialist": data[3],
                     });
                 }
 
@@ -71,25 +76,60 @@ module.exports = {
             });
 
             ///////////////////////////////////////////////////////////////////////////////
-            option.fromPath(uploadedFiles[1].fd, { headers: false }).on("data", function (data) {
-                if (index == 0 || index == 1) {
+          
+            var uploadedFiles = await req.file('optionmap').upload({ maxBytes: 10000000 });
+
+            if (uploadedFiles) { return res.badRequest('No file was uploaded'); }
+
+            var index = 0;
+            var batch = db.batch();
+
+            csv.fromPath(uploadedFiles[1].fd, { headers: false }).on("data", function (data) {
+                if (index == 0 || data == 1) {
                     index++;
                     return;
                 }
 
+                
                 //console.log()
-                //console.log(data);
+                console.log(data);
                 if (data[1].includes('General')) {
 
-                    var count = 3;
-                    batch.set(db.collection('general').doc(option).collection('data[2]').doc(count), {
+                    var count = 5;
+                    var optionNum = 1;
+                    while(data[count]){
+                    batch.set(db.collection('general').doc(option).collection('data[2]').doc(optionNum), {
                         "content": data[count],
                         // "percentage": data[],
                         // "price": data[],
                         // "title" : data[],
                         "內容": data[count],
-                    });
+                    }
+                );
+                count++;
+                optionNum++;
+                    }
                 }
+
+                else if(data[0]){
+                    var surgeryNum = daat[2].match(/\d/g);
+                    var count = 5;
+                    var optionNum = 1;
+                    while(data[count]){
+                    batch.set(db.collection('surgery').doc(option).collection(surgeryNum).doc('specific').collection(data[2].doc(optionNum).collection(count)), {
+                        "content": data[count],
+                        // "percentage": data[],
+                        // "price": data[],
+                        // "title" : data[],
+                        "內容": data[count],
+                    }
+                );
+                count++;
+                optionNum++;
+                    }
+                }
+
+
 
                 index++;
             }).on("end", function () {
@@ -100,9 +140,9 @@ module.exports = {
 
             });
         },
-},
-
 };
+
+//};
 //...
 
 
