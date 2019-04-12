555f0a8f215def95632badae1c6a154e0588a08a
jenniferjenniferjen <16226437@life.hkbu.edu.hk>
2019-04-12 17:56:30 +0800

    calibration testing

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -79,19 +79,18 @@ module.exports = {
         const csv = require("fast-csv");
         var db = sails.firebaseAdmin.firestore();
 
-        var surgerynoBuffer = new Buffer(req.body.surgeryno[1].split(",")[1], 'base64').toString('utf8');
-        var optionmapBuffer = new Buffer(req.body.optionmap[1].split(",")[1], 'base64').toString('utf8');
+        // var surgerynoBuffer = new Buffer(req.body.surgeryno[1].split(",")[1], 'base64').toString('utf8');
+        // var optionmapBuffer = new Buffer(req.body.optionmap[1].split(",")[1], 'base64').toString('utf8');
         var calibrationBuffer = new Buffer(req.body.calibration[1].split(",")[1], 'base64').toString('utf8');
-        var hospitalBuffer = new Buffer(req.body.hospital[1].split(",")[1], 'base64').toString('utf8');
+        // var hospitalBuffer = new Buffer(req.body.hospital[1].split(",")[1], 'base64').toString('utf8');
 
         //////////////////////CALIBRATION//////////////////////////////////////
 
-        ///////////////////////HOSIPITAL CODE////////////////////////////
         await new Promise((resolve, reject) => {
             var index = 0;
             var batch = db.batch();
-       
-            csv.fromString(hospitalBuffer, { headers: false }).on("data", function (data) {
+
+            csv.fromString(calibrationBuffer, { headers: false }).on("data", function (data) {
                 if (index == 0) {
                     index++;
                     return;
@@ -103,27 +102,101 @@ module.exports = {
 
                 // console.log()
                 console.log(data);
-                if (data[0]&& data[1]) {
+                if (data[4]) {
+                    /////////////extract surgery number & surgery option//////////
+                    //data[4] = name 
 
-                    batch.set(db.collection('hospital').doc(data[0]), {
-                        "id": data[0],
-                        "content": data[1],
-                        "內容": data[1],
-        
-                    });
+                    var cSurgeryNo = data[6].toLowerCase().match(/^([\d]+)/g)[0];//take out surgery number e.g. 121
+                    var cSurgeryOptionNum = data[6].toLowerCase().match(/([\d]+)$/g)[0];//take out surgery option number e.g. 1 from A1
+                    var cSurgeryOption = data[6].toLowerCase().replace(cSurgeryNo, '').replace(cSurgeryOptionNum, '');//take out the option GeneralA/A(specific)
+
+
+                    var amount = data[7];//price
+                    var agePercentage = data[6].match(/^[\d]+/g);//extract digits but no %
+                    var useAmount = data[44].match(/\d/g);//extract digits but no %
+
+                    // var d = {};//no need this as read line by line
+
+                    if (cSurgeryNo) {
+
+                        ////////////Base Case/////////////
+                        if (data[4].toLowerCase().includes("base")) {
+                            // d['cSurgeryNo'] = data[7];
+                            batch.set(db.collection('surgery').doc(cSurgeryNo).collection('option'), {
+                                "baseline_price": data[7],
+                            }, {merge:true});//overwrite the field
+                        } else if (data[4].toLowerCase().includes("lower")) {
+                            // d['cSurgeryNo'] = data[7];
+                            batch.set(db.collection('surgery').doc('option'), {
+                                "lowerBaselinePrice": data[7],
+                            }, {merge:true});
+                        } else if (data[4].toLowerCase().includes("upper")) {
+                            batch.set(db.collection('surgery').doc('option'), {
+                                "upperBaselinePrice": data[7],
+                            }, {merge:true});
+                        } else if(cSurgeryOptionNum) {
+                            if (data[6].toLowerCase().includes('general')) {
+
+                            } else {
+                                batch.set(db.collection('surgery').doc(cSurgeryNo).collection('option').doc('specific').collection(cSurgeryOption).doc(cSurgeryOptionNum), {
+                                    // "baseline_price": data[7],
+                                });
+                            }
+                        }
+
+                    }
+
+                    //////////////////////////////////////////
+
                 }
 
                 index++;
             }).on("end", function () {
                 batch.commit().then(function () {
-                    console.log('hospital end');
+                    console.log('calibration end');
                     resolve();
                 });
 
             });
         });
+        ///////////////////////HOSIPITAL CODE////////////////////////////
+        // await new Promise((resolve, reject) => {
+        //     var index = 0;
+        //     var batch = db.batch();
 
-       
+        //     csv.fromString(hospitalBuffer, { headers: false }).on("data", function (data) {
+        //         if (index == 0) {
+        //             index++;
+        //             return;
+        //         }
+        //         if (index == 1) {
+        //             index++;
+        //             return;
+        //         }
+
+        //         // console.log()
+        //         console.log(data);
+        //         if (data[0] && data[1]) {
+
+        //             batch.set(db.collection('hospital').doc(data[0]), {
+        //                 "id": data[0],
+        //                 "content": data[1],
+        //                 "內容": data[1],
+
+        //             });
+        //         }
+
+        //         index++;
+        //     }).on("end", function () {
+        //         batch.commit().then(function () {
+        //             console.log('hospital end');
+        //             resolve();
+        //         });
+
+        //     });
+        // });
+
+
         console.log(req.body);
 
         return res.ok("Files uploaded successfully");
